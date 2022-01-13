2eac6fd3126409d98ac1cccbbf3c801109b389b3
Bintang73 <42708989+Bintang73@users.noreply.github.com>
2022-01-13 14:43:52 +0700

    Delete views directory

--- views/beranda.php
+++
@@ -1,7 +0,0 @@
-<?php
-?>
-<div class="halaman">
-<h2>Selamat datang di Toko Buah</h2>
-<p>Toko ini menyediakan berbagai macam buah segar yang diambil langsung dari perkebunan.</p>
-<img src="https://c1.wallpaperflare.com/preview/939/765/130/fruit-vegetable-market-marketplace.jpg" height="350px" width="600px;">
-</div>
